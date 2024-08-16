c5b8b10e5f6c6a7ab294a671949656461ddd973b
カムイ <178002956+kamui72@users.noreply.github.com>
2024-08-16 21:45:55 +0900

    [fix] countupに関するメソッドを削除

--- app/StopWatchActivityHandler.py
+++ app/StopWatchActivityHandler.py
@@ -7,29 +7,9 @@ class StopWatchActivityHandler:
         # ストップウォッチを開始
         self.stopwatch.start()
 
-    def _start_counting(self):
-        # ストップウォッチが動作中であれば
-        if self.stopwatch.is_running():
-            # ストップウォッチの時間を1ミリ秒進める
-            self.stopwatch.count_up()
-            # 次のカウントを1ミリ秒後にスケジュールする
-            self._schedule_next_count()
-
-    def _schedule_next_count(self):
-        # UIフレームワークに依存するが、1ミリ秒後に再度_countingを実行するタイマーをセットする
-        # 例えば、Tkinterではafterメソッドを使うことができる
-        pass  # 実装が必要
-
     def stop(self):
         # ストップウォッチを停止
         self.stopwatch.stop()
-        # カウントアップを止めるための内部メソッドを呼び出す
-        self._cancel_counting()
-
-    def _cancel_counting(self):
-        # スケジュールされた次のカウントアップ呼び出しをキャンセルする
-        # 実際のUIフレームワークの機能に依存する
-        pass  # 実装が必要
 
     def rap_split(self):
         # スプリットタイムを記録する
