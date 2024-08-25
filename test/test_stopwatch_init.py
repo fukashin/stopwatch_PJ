b6921bba337b0478d950e6d6bcd93369f2bd7243
itigo-ame <178281024+itigo-ame@users.noreply.github.com>
2024-08-25 10:45:54 +0900

    [add]test_stopwatch_init.pyファイルの追加と__init__のテスト

---
+++ test/test_stopwatch_init.py
@@ -0,0 +1,20 @@
+from app.stopwatch import StopWatch
+
+stopwatch = StopWatch()
+
+if stopwatch.elapsed_time != 0:
+    raise Exception("elapsed_timeが正しく初期化されていません")
+
+if stopwatch.is_running:
+    raise Exception("is_runningが正しく初期化されていません")
+
+if stopwatch.rap_time != []:
+    raise Exception("rap_timeが正しく初期化されていません")
+
+if stopwatch.split_time != []:
+    raise Exception("split_timeが正しく初期化されていません")
+
+if stopwatch.display_time != "00:00:00.00":
+    raise Exception("display_timeが正しく初期化されていません")
+
+print("テストに合格しました")
