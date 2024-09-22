ca9047416f5fda720041542b740a688cee0f417e
itigo-ame <178281024+itigo-ame@users.noreply.github.com>
2024-09-22 10:58:42 +0900

    [fix]stopwatch.reset()の仕様変更に伴う修正

--- test/test_stopwatch_reset.py
+++ test/test_stopwatch_reset.py
@@ -8,26 +8,35 @@ def check_parameter():
     check_is_running = stopwatch.is_running == False
     check_rap_time = stopwatch.rap_time == []
     check_split_time = stopwatch.split_time == []
-    check_display_time = stopwatch.display_time == "00:00:00.00"
-    return check_elapsed_time & check_is_running & check_rap_time & check_split_time & check_display_time
+    check_display_time = stopwatch.display_time == ["00:00:00", ".00"]
+    check_rap_display_time = stopwatch.rap_display_time == []
+    check_sprit_display_time = stopwatch.split_display_time == []
+    return check_elapsed_time & check_is_running & check_rap_time & check_split_time & check_display_time & check_rap_display_time & check_sprit_display_time
 
+
+# is_runningがFalseの時のテスト
 stopwatch.elapsed_time = 1231
 stopwatch.is_running = False
-stopwatch.rap_time: list = []
-stopwatch.split_time: list = []
-stopwatch.display_time = "00:00:21:11"
+stopwatch.rap_time: list = [1231]
+stopwatch.split_time: list = [1231]
+stopwatch.display_time = ["00:00:21", ".14"]
+stopwatch.rap_display_time: list = ["00:00:21.14"]
+stopwatch.split_display_time: list = ["00:00:21.14"]
 stopwatch.reset()
 
 if not check_parameter():
     raise Exception("resetの挙動が正しくありません")
 
+# is_runningがTrueの時のテスト
 check_exception: bool
 
 stopwatch.elapsed_time = 1231
 stopwatch.is_running = True
-stopwatch.rap_time: list = []
-stopwatch.split_time: list = []
+stopwatch.rap_time: list = ["00:00:21:11"]
+stopwatch.split_time: list = ["00:00:21:11"]
 stopwatch.display_time = "00:00:21:11"
+stopwatch.rap_display_time: list = ["00:00:21:11.14"]
+stopwatch.split_display_time: list = ["00:00:21:11.14"]
 
 try:
     stopwatch.reset()
