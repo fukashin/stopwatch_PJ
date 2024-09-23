62e1618f7323e7b166b149b2309fd2aa26c21728
itigo-ame <178281024+itigo-ame@users.noreply.github.com>
2024-09-23 12:44:46 +0900

    [fix]エラー文の修正

--- test/test_stopwatch_set_rap_display_time.py
+++ test/test_stopwatch_set_rap_display_time.py
@@ -11,19 +11,19 @@ stopwatch.set_rap_display_time(set_display_time_1)
 stopwatch.set_rap_display_time(set_display_time_2)
 
 if set_display_time_0 != stopwatch.rap_display_time[0]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 if set_display_time_1 != stopwatch.rap_display_time[1]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 if set_display_time_2 != stopwatch.rap_display_time[2]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 
 stopwatch.set_rap_display_time(set_display_time_3)
 
 if set_display_time_1 != stopwatch.rap_display_time[0]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 if set_display_time_2 != stopwatch.rap_display_time[1]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 if set_display_time_3 != stopwatch.rap_display_time[2]:
-    raise Exception("set_split_display_timeの挙動が正しくありません")
+    raise Exception("set_rap_display_timeの挙動が正しくありません")
 
 print("テストに合格しました")
